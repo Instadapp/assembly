c1c7f0c533badcebd7facb3188e52a151cf4104e
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2021-08-14 15:11:20 +0300

    update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,10 +12,27 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: "msapplication-TileColor", content: "#ffffff" },
+      { name: "msapplication-TileImage", content: "/ms-icon-144x144.png" },
+      { name: "theme-color", content: "#ffffff" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "apple-touch-icon", sizes: "57x57", href: "/apple-icon-57x57.png" },
+      { rel: "apple-touch-icon", sizes: "60x60", href: "/apple-icon-60x60.png" },
+      { rel: "apple-touch-icon", sizes: "72x72", href: "/apple-icon-72x72.png" },
+      { rel: "apple-touch-icon", sizes: "76x76", href: "/apple-icon-76x76.png" },
+      { rel: "apple-touch-icon", sizes: "114x114", href: "/apple-icon-114x114.png" },
+      { rel: "apple-touch-icon", sizes: "120x120", href: "/apple-icon-120x120.png" },
+      { rel: "apple-touch-icon", sizes: "144x144", href: "/apple-icon-144x144.png" },
+      { rel: "apple-touch-icon", sizes: "152x152", href: "/apple-icon-152x152.png" },
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-icon-180x180.png" },
+      { rel: "icon", type: "image/png", sizes: "192x192", href: "/android-icon-192x192.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon-96x96.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+      { rel: "manifest", href: "/manifest.json" },
     ]
   },
 
@@ -45,7 +62,7 @@ export default {
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/composition-api/module',   
+    '@nuxtjs/composition-api/module',
     // Doc: https://github.com/nuxt-community/google-fonts-module
     "@nuxtjs/google-fonts",
     "@nuxtjs/svg",
