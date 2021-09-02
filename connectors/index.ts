064f21a9d8f57d21bdc46d1eda15c0001d5c4cfb
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2021-09-03 01:19:19 +0300

    wip

--- connectors/index.ts
+++ connectors/index.ts
@@ -1,6 +1,10 @@
 import { setWeb3LibraryCallback } from "@kabbouchi/vue-web3";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { PortisConnector } from "@web3-react/portis-connector";
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+
+import INSTADAPP_LOGO_URL from "~/assets/logo/instadapp-logo-icon.svg?inline";
 
 import Web3 from "web3";
 
@@ -16,4 +20,17 @@ export const walletconnect = new WalletConnectConnector({
     137: "https://rpc-mainnet.maticvigil.com"
   },
   supportedChainIds: [1, 137]
+});
+
+
+// mainnet only
+export const portis = new PortisConnector({
+  dAppId: process.env.PORTIS_ID,
+  networks: [1]
+});
+
+export const walletlink = new WalletLinkConnector({
+  url: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`,
+  appName: "Instadapp",
+  appLogoUrl: INSTADAPP_LOGO_URL
 });
