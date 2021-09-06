bd8a7f0e80ff023bd4ff3ecf1be25b1f309a1e99
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2021-09-06 19:47:55 +0300

    Update useSafeAppConnection.ts

--- composables/useSafeAppConnection.ts
+++ composables/useSafeAppConnection.ts
@@ -2,7 +2,7 @@ import { useWeb3 } from "@instadapp/vue-web3";
 import { injected } from "../connectors";
 import { onMounted, ref, watch } from "@nuxtjs/composition-api";
 import { SafeAppConnector } from "@gnosis.pm/safe-apps-web3-react";
-import { useNetwork } from "./useNetwork";
+import { Network, useNetwork } from "./useNetwork";
 
 export function useSafeAppConnection(connector?: SafeAppConnector) {
   const { activate, active } = useWeb3();
@@ -17,8 +17,7 @@ export function useSafeAppConnection(connector?: SafeAppConnector) {
           tried.value = true;
         });
 
-        //@ts-ignore
-        activeNetworkId.value = await connector.getChainId()
+        activeNetworkId.value = (await connector.getChainId() === 1) ? Network.Mainnet : Network.Polygon;
       } else {
         tried.value = true;
       }
