e8122a6e0aa078b26bf130cde8dd9363b92bda85
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2021-09-07 20:00:30 +0300

    Custom ledger connector

--- connectors/index.ts
+++ connectors/index.ts
@@ -3,7 +3,8 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { PortisConnector } from "@web3-react/portis-connector";
 import { WalletLinkConnector } from "@web3-react/walletlink-connector";
-import { LedgerConnector } from "@web3-react/ledger-connector";
+// import { LedgerConnector } from "@web3-react/ledger-connector";
+import { LedgerConnector } from "./ledger-connector";
 
 import INSTADAPP_LOGO_URL from "~/assets/logo/instadapp-logo-icon.svg?inline";
 
@@ -48,8 +49,17 @@ export { gnosisSafe };
 
 const POLLING_INTERVAL = 12000;
 
+export enum LedgerDerivationPath {
+  "Legacy" = "44'/60'/0'/x",
+  "LedgerLive" = "44'/60'/x'/0/0"
+}
+
 export const ledger = new LedgerConnector({
   chainId: 1,
   url: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`,
-  pollingInterval: POLLING_INTERVAL
+  requestTimeoutMs: 60,
+  pollingInterval: POLLING_INTERVAL,
+  baseDerivationPath: "44'/60'/x'/0/0",
+  accountsOffset: 0,
+  accountsLength: 1
 });
