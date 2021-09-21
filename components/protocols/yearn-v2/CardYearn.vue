0926447767fa957d54904859e0067082a7922b67
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2021-09-21 23:11:08 +0300

    yearn

---
+++ components/protocols/yearn-v2/CardYearn.vue
@@ -0,0 +1,100 @@
+<template>
+  <div
+    class="flex-shrink-0 bg-white rounded-lg relative flex flex-col flex-1 px-4 pt-4 pb-6 dark:bg-dark-500"
+    style="box-shadow: -1px -3px 10px rgba(12, 25, 91, 0.03), 2px 4px 12px rgba(12, 25, 91, 0.05)"
+  >
+    <div class="flex items-center h-14">
+      <div class="flex mr-4 -space-x-3 overflow-hidden">
+        <IconCurrency :currency="tokenKey" class="w-12 h-12" no-height />
+      </div>
+
+      <div class="flex flex-col flex-grow">
+        <div class="mb-1 font-medium leading-none whitespace-no-wrap text-19">
+          {{ formatUsd(supplyUsd) }}
+        </div>
+        <div class="flex leading-none whitespace-no-wrap">
+          <span class="text-grey-pure text-14"
+            >{{ formatDecimal(supply) }} {{ symbol }}</span
+          >
+          <Info
+            :text="`${formatUsd(priceInUsd, 2)}/${symbol}`"
+            icon="price"
+            class="ml-1"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="grid w-full grid-cols-3 gap-4 mt-4">
+      {{ formatPercent(netAPY) }} net APY
+    </div>
+
+    <hr class="mt-4" />
+
+    <div class="flex items-center justify-around mt-6">
+      <button
+        class="mr-4 h-10 w-full bg-primary-blue-dark shadow text-white rounded-[4px] hover:bg-primary-blue-hover"
+        @click="showSupply"
+      >
+        Supply
+      </button>
+      <button
+        class="h-10 w-full text-primary-blue-dark shadow border border-primary-blue-dark hover:border-primary-blue-hover rounded-[4px] hover:text-primary-blue-hover"
+        @click="showWithdraw"
+      >
+        Withdraw
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed, defineComponent, useContext } from "@nuxtjs/composition-api";
+import { useFormatting } from "~/composables/useFormatting";
+import { useToken } from "~/composables/useToken";
+
+export default defineComponent({
+  props: {
+    tokenKey: { type: String, required: true },
+    vault: { type: String, default: null },
+    supply: { type: String, required: true },
+    supplyUsd: { type: String, required: true },
+    priceInUsd: { type: String, default: "0" },
+    netAPY: { type: String, default: "0" }
+  },
+
+  setup(props) {
+    const { app } = useContext();
+    const { formatPercent, formatUsd, formatDecimal } = useFormatting();
+    const { getTokenByKey } = useToken();
+
+    const symbol = computed(
+      () => getTokenByKey(props.tokenKey)?.symbol || props.tokenKey
+    );
+
+    function showSupply() {
+      app.router.push({ hash: `supply?vault=${props.vault}` });
+    }
+
+    function showWithdraw() {
+      app.router.push({ hash: `withdraw?vault=${props.vault}` });
+    }
+
+    return {
+      showSupply,
+      showWithdraw,
+      formatPercent,
+      formatUsd,
+      formatDecimal,
+      symbol
+    };
+  }
+});
+</script>
+<style scoped>
+.position-button {
+  @apply flex-1;
+  @apply h-8;
+  @apply h-8;
+}
+</style>
---
+++ composables/protocols/useYearnV2Position.ts
@@ -0,0 +1,131 @@
+import { useWeb3 } from "@instadapp/vue-web3";
+import { ref, useContext, watch } from "@nuxtjs/composition-api";
+import addresses from "~/constant/addresses";
+import tokens from "~/constant/tokens";
+import { useDSA } from "../useDSA";
+import yearnV2ABI from "~/abis/read/yearnV2.json";
+import useEventBus from "../useEventBus";
+import BigNumber from "bignumber.js";
+import { useBigNumber } from "../useBigNumber";
+
+const resolver = addresses.mainnet.resolver.yearnV2;
+
+const wantAddresses = [
+  tokens.mainnet.getTokenByKey("weth").address,
+  tokens.mainnet.getTokenByKey("dai").address
+];
+
+const vaults = ref([]);
+
+export function useYearnV2Position() {
+  const { $axios } = useContext();
+  const { times } = useBigNumber();
+  const { library } = useWeb3();
+  const { activeAccount } = useDSA();
+  const { onEvent } = useEventBus();
+
+  const fetchPosition = async () => {
+    vaults.value = [];
+    const availableVaults = await $axios
+      .$get("https://api.yearn.finance/v1/chains/1/vaults/all")
+      .then(vs => vs.filter(v => v.type === "v2"));
+
+    if (!library.value) {
+      return;
+    }
+
+    if (!activeAccount.value) {
+      return;
+    }
+
+    const resolverInstance = new library.value.eth.Contract(
+      yearnV2ABI as any,
+      resolver
+    );
+
+    const tokensArr = wantAddresses; // tokens.mainnet.allTokens.map(a => a.address);
+
+    const rawData = await resolverInstance.methods
+      .getPositionsForLatest(activeAccount.value.address, tokensArr)
+      .call();
+
+    const newVaults = [];
+
+    rawData.forEach(
+      ([
+        vaultLatestVersion,
+        vault,
+        want,
+        pricePerShare,
+        availableDepositLimit,
+        totalAssets,
+        balanceOf,
+        wantBalanceOf,
+        expectedShareValue,
+        decimals,
+        isDeprecated,
+        emergencyShutdown
+      ]) => {
+        const v = availableVaults.find(v => v.address === vault);
+        if (v) {
+          const supply = new BigNumber(balanceOf)
+          .dividedBy(10 ** decimals)
+          .toFixed();
+
+          newVaults.push({
+            ...v,
+            priceInUsd: v.tvl.price.toString(),
+            position: {
+              vaultLatestVersion,
+              vault,
+              want,
+              pricePerShare,
+              availableDepositLimit,
+              totalAssets,
+              balanceOf,
+              wantBalanceOf,
+              expectedShareValue,
+              decimals,
+              isDeprecated,
+              emergencyShutdown,
+              supply,
+              supplyUsd: times(supply, v.tvl.price).toFixed(),
+            }
+          });
+        }
+      }
+    );
+    vaults.value = newVaults;
+  };
+
+  const refreshPosition = async () => {
+    await fetchPosition();
+  };
+
+  onEvent("protocol::compound::refresh", refreshPosition);
+
+  watch(
+    library,
+    async val => {
+      if (val) {
+        refreshPosition();
+      }
+    },
+    { immediate: true }
+  );
+
+  watch(
+    activeAccount,
+    async val => {
+      if (val) {
+        refreshPosition();
+      }
+    },
+    { immediate: true }
+  );
+
+  return {
+    vaults,
+    refreshPosition
+  };
+}
